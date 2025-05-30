e25c8adf1e1c13451c8baf92d79c23ff9999f8fc
GabrielAcker <gabriel.acker@gmail.com>
2025-05-30 00:29:19 -0300

    atualizar servidor

--- routers/dealRoutes.php
+++ routers/dealRoutes.php
@@ -4,13 +4,13 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 require_once __DIR__ . '/../controllers/DealController.php';
 
-if ($uri === '/apps/deal/criar' && $method === 'POST') {
+if ($uri === '/criar' && $method === 'POST') {
     (new DealController())->criar();
-} elseif ($uri === '/apps/deal/consultar' && $method === 'GET') {
+} elseif ($uri === '/consultar' && $method === 'GET') {
     (new DealController())->consultar();
-} elseif ($uri === '/apps/deal/editar' && $method === 'POST') {
+} elseif ($uri === '/editar' && $method === 'POST') {
     (new DealController())->editar();
 } else {
     http_response_code(404);
-    echo json_encode(['erro' => 'Rota não encontrada em /deal']);
+    echo json_encode(['erro' => 'Rota não encontrada']);
 }
