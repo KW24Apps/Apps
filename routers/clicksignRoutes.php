9f0bb48bb30de80ad8c373c5a0e2651a2f667c53
GabrielAcker <gabriel.acker@gmail.com>
2025-06-04 01:04:41 -0300

    atualizar servidor

--- routers/clicksignRoutes.php
+++ routers/clicksignRoutes.php
@@ -1,4 +1,6 @@
 <?php
+file_put_contents(__DIR__ . '/../logs/debug_routes.log', "[ROTA] URI: $uri | METHOD: $method" . PHP_EOL, FILE_APPEND);
+
 $uri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $method = $_SERVER['REQUEST_METHOD'];
 
