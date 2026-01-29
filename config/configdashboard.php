ad5486876d28539762aa2efb17ae7a0f56ed52ea
GabrielAcker <gabriel.acker@gmail.com>
2026-01-29 01:32:16 -0300

    Comiit para novo Servidor VPS Teste 04

--- config/configdashboard.php
+++ config/configdashboard.php
@@ -3,7 +3,7 @@
 // Configuração específica para o dashboard
 // Sempre usar credenciais de produção, pois o dashboard roda no servidor
 $dashboardConfig = [
-    'host' => 'localhost',
+    'host' => '162.214.97.76',
     'dbname' => 'kw24co49_api_kwconfig',
     'usuario' => 'kw24co49_kw24',
     'senha' => 'BlFOyf%X}#jXwrR-vi'
